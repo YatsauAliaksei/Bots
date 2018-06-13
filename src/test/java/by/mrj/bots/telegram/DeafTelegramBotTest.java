1d2252bb4aceccc74641d2f2c4093af65e90a1ef
Alexey <yatsew.alexey@gmail.com>
2018-06-13 19:25:02 +0200

    test improved.

--- src/test/java/by/mrj/bots/telegram/DeafTelegramBotTest.java
+++ src/test/java/by/mrj/bots/telegram/DeafTelegramBotTest.java
@@ -1,13 +1,18 @@
 package by.mrj.bots.telegram;
 
 import org.junit.Test;
+import org.telegram.telegrambots.api.objects.Message;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class DeafTelegramBotTest {
 
     @Test
     public void say() {
         TelegramBot telegramBot = TelegramBot.takeMe(DeafTelegramBot.class, "firstOne123bot");
-        telegramBot.say("Hello, man");
+        Message message = telegramBot.say("Hello, man");
+        assertThat(message).isNotNull();
+        assertThat(message.getMessageId()).isNotNull();
     }
 
 }
